e11581d11b9da251ffda45ce45288badb3600022
VasylievHennadii <vga.kha@gmail.com>
2020-12-16 00:08:22 +0200

    подключение ЧПУ

--- config/web.php
+++ config/web.php
@@ -15,6 +15,7 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'BIdAvmB3CmxosOUA105F4fFQXPPYuGfb',
+            'baseUrl' => '',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -42,15 +43,14 @@ $config = [
                 ],
             ],
         ],
-        'db' => $db,
-        /*
+        'db' => $db,        
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
+            'enableStrictParsing' => false,
             'rules' => [
             ],
-        ],
-        */
+        ],        
     ],
     'params' => $params,
 ];
